6bd167795b227220a082da1a65bda845829b67ef
NoAkumu <nakumu90@gmail.com>
2026-03-06 23:43:46 -0300

    Updated:   modules/Doujin.js     Removed some hardcoded strings, and splitted the main function into 2 methods

--- modules/Doujin.js
+++ modules/Doujin.js
@@ -4,22 +4,26 @@ const nhentai = require("nhentai");
 const api = new nhentai.API();
 
 module.exports = {
-    async thegreatflood(channel) {
-        const tag = "*";
+    async GetDoujin(tag) {
+        const tag = tag;
         const page = Math.floor((await api.search(tag).finally().catch(console.error())).numPages * Math.random());
-        const page_chosen = (await api.search(tag, {page:page}).finally()).doujins
-        const doujin_chosen = page_chosen[Math.floor(Math.random() * page_chosen.length)]
+        const page_chosen = (await api.search(tag, {page:page}).finally()).doujins;
+        const doujin_chosen = page_chosen[Math.floor(Math.random() * page_chosen.length)];
 
+        return doujin_chosen;
+    },
+    async CreateDoEmbed() {
+        const nhentai_icon = 'https://nhentai.net/static/favicon-32x32.png'
         const m_Embed = new EmbedBuilder()
             .setColor(Colors.Red)
             .setTitle(doujin_chosen.titles.english)
             .setURL(doujin_chosen.url)
-            .setAuthor({ name: doujin_chosen.tags.artists.map(value => `${value.name}`).join(", "), iconURL: 'https://nhentai.net/static/favicon-32x32.png', url: 'https://nhentai.net/' })
+            .setAuthor({ name: doujin_chosen.tags.artists.map(value => `${value.name}`).join(", "), iconURL: nhentai_icon, url: 'https://nhentai.net/' })
             .setDescription(doujin_chosen.tags.tags.map(value => `${value.name}`).join(", "))
-            .setThumbnail('https://nhentai.net/static/favicon-32x32.png')
+            .setThumbnail(nhentai_icon)
             .setImage(doujin_chosen.cover.url)
             .setTimestamp(doujin_chosen.uploadTimestamp)
-            .setFooter({ text: "Likes:"+doujin_chosen.favorites, iconURL: 'https://nhentai.net/static/favicon-32x32.png' });
-        await channel.send({embeds: [m_Embed]});
+            .setFooter({ text: "Likes:"+doujin_chosen.favorites, iconURL: nhentai_icon });
+        return m_Embed;
     }
 }
