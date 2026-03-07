d5d325ebb817dca192df2b626a0de18e96a0b31d
NoAkumu <nakumu90@gmail.com>
2026-03-06 23:39:01 -0300

    modified:   index.js 	renamed:    flood.js -> modules/GetDoujin_Embed.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { thegreatflood } = require("./flood.js");
+const { thegreatflood } = require("./modules/GetDoujin_Embed.js");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { Keyv } = require("keyv");
 
